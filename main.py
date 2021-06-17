41f0d73c223b962be86378974b047ee443a1775b
MAN TV MASS <32133224+mantvmass@users.noreply.github.com>
2021-06-17 23:08:22 +0700

    new update!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from pymessenger.bot import Bot
 import time
-from weather import key1, key2, key3, key4, key5, key6, key7, key8, key9, key10, key11, key12, welcome
+from weather import key1, key2, key3, key4, key5, key6, key7, key8, key9, key10, key11, key12, key13, welcome
 
 
 
@@ -35,7 +35,7 @@ def bot_messenger():
 
                     if data['message'].get('text'):
                         msg = data['message']['text']
-                        print(f"ข้อความที่ได้รับคือ ---- {msg}")
+                        print(f"message: {msg}")
 
                         if msg == key1 or msg == key2 or msg == key3 or msg == key4 or msg == key5 or msg == key6 or msg == key7 or msg == key8 or msg == key9 or msg == key10 or msg == key11 or msg == key12:                            
                             bot.send_text_message(recipient_id, welcome)
