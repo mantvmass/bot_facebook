71ba04983692fd6509aeafd509d8e073bd3f63b2
MAN TV MASS <32133224+mantvmass@users.noreply.github.com>
2021-07-13 20:53:18 +0700

    update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Facebook: https://www.facebook.com/PhuminMaliwan
 from flask import Flask, request
 from pymessenger.bot import Bot
 import time
-from weather import key1, key2, key3, key4, key5, key6, key7, key8, key9, key10, key11, key12, key13, welcome, dev
+from weather import *
 
 
 app = Flask(__name__)
